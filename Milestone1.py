173bd879e24a2e307583e8ba074749d7a34fd177
Evelyn Schwartz <evelynschwartz2@gmail.com>
2026-09-11 14:13:19 -0400

    Finished the Store class with the following methods: add_product, find_product, add_customer, and find_customer.

--- Milestone1.py
+++ Milestone1.py
@@ -16,9 +16,32 @@ class ShopingCart:
 class Store:
     
     def __init__(self):
+        self.products = []
+        self.customers = []
 
-    def add_product(self, test):
+    def add_product(self, product: 'Product') -> bool:
+        if self.find_product(product.id) is None:
+            self.products.append(product)
+            return True
+        return False
 
+    def find_product(self, product_id: str) -> 'Product':
+        for p in self.products:
+            if p.id == product_id:
+                return p
+        return None
+
+    def add_customer(self, customer: 'Customer') -> bool:
+        if self.find_customer(customer.id) is None:
+            self.customers.append(customer)
+            return True
+        return False
+
+    def find_customer(self, customer_id: str) -> 'Customer':
+        for c in self.customers:
+            if c.id == customer_id:
+                return c
+        return None
 
 
 
