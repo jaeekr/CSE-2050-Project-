85c50bd4afc9bca220def28d0c93f59af1b912cd
Jaee Raut <jaeekr@gmail.com>
2026-09-11 12:35:37 -0400

    Add files via upload

---
+++ Milestone1.py
@@ -0,0 +1,23 @@
+class Product:
+
+    def __init__():
+        pass
+
+class Customer:
+
+    def __init__():
+            pass
+
+class ShopingCart:
+    
+    def __init__():
+            pass
+
+class Store:
+    
+    def __init__():
+            pass
+
+
+
+
