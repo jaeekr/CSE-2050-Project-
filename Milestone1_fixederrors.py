becfd71bbad654209d5e6e89c96e4a1c540f3639
Jaee Raut <jaeekr@gmail.com>
2026-09-12 18:13:37 -0400

    Fixed some errors in milestone1
    Before, the example given on the milestone PDF was not working, and after some edits, the example now works .

---
+++ Milestone1_fixederrors.py
@@ -0,0 +1,107 @@
+class Product:
+
+    def __init__(self, product_id:str, name:str, price:float):
+        self.product_id=product_id 
+        self.name=name
+        self.price= price
+
+        
+    def get_id(self):
+        return self.product_id
+    
+    def get_name(self):
+        return self.name
+
+    def get_price(self):
+        return self.price
+
+class Customer:
+
+    def __init__(self, customer_id:str, name:str):
+            self.customer_id= customer_id
+            self.name= name
+            self.cart= ShoppingCart()
+
+    def get_id(self):
+            return self.customer_id
+    
+    def get_name(self):
+            return self._name
+    
+    def get_cart(self):
+            return self.cart
+
+class ShoppingCart:
+    
+    def __init__(self):
+        self.cart = []
+
+    def add_product(self, product: 'Product'):
+        self.cart.append(product)
+
+    def product(self, product_id: str):
+        for product in self.cart:
+            if product.get_id() == product_id:
+                self.cart.remove(product)
+                return True       
+        return False       
+            
+    def get_items(self):
+        return f'{self.cart}'
+
+    def calculate_total(self):
+        total = 0
+        for product in self.cart:
+            total += product.get_price()
+
+        return total
+
+    def is_empty(self):
+        if len(self.cart) == 0:
+            return True
+        else:
+            return False
+
+class Store:
+    
+    def __init__(self):
+        self.products = []
+        self.customers = []
+
+    def add_product(self, product: 'Product') -> bool:
+        if self.find_product(product.get_id()) is None:
+            self.products.append(product)
+            return True
+        return False
+
+    def find_product(self, product_id: str) -> 'Product':
+        for p in self.products:
+            if p.get_id() == product_id:
+                return p
+        return None
+
+    def add_customer(self, customer: 'Customer') -> bool:
+        if self.find_customer(customer.get_id()) is None:
+            self.customers.append(customer)
+            return True
+        return False
+
+    def find_customer(self, customer_id: str) -> 'Customer':
+        for c in self.customers:
+            if c.id == customer_id:
+                return c
+        return None
+
+
+
+store = Store()
+mouse = Product("P100", "Wireless Mouse", 29.99)
+keyboard = Product("P101", "Keyboard", 59.99)
+store.add_product(mouse)
+store.add_product(keyboard)
+customer = Customer("C100", "Alex")
+store.add_customer(customer)
+cart = customer.get_cart()
+cart.add_product(mouse)
+cart.add_product(keyboard)
+print(cart.calculate_total())
