f98f326dd8d1f4c80d2402cc7cdd9c696e0ccbc5
emiliofigueroa1 <157066769+emiliofigueroa1@users.noreply.github.com>
2026-09-12 11:54:26 -0400

    Add ShoppingCart class

--- Milestone1.py
+++ Milestone1.py
@@ -34,7 +34,33 @@ class Customer:
 class ShoppingCart:
     
     def __init__(self):
-            pass
+        self.cart = []
+
+    def add_product(self, product: 'Product'):
+        self.cart.append(product)
+
+    def product(self, product_id: str):
+        for product in self.cart:
+            if product.get_id() == product_id:
+                self.cart.remove(product)
+                return True       
+        return False       
+            
+    def get_items(self):
+        return f'{self.cart}'
+
+    def calculate_total(self):
+        total = 0
+        for product in self.cart:
+            total += product.get_price()
+
+        return total
+
+    def is_empty(self):
+        if len(self.cart) == 0:
+            return True
+        else:
+            return False
 
 class Store:
     
