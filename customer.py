61e13e2657f462e9504a4ca9ea75e382d946d119
Jaee Raut <jaeekr@gmail.com>
2026-09-14 14:43:00 -0400

    Rename parameter '_id' to 'id' in Customer class

--- customer.py
+++ customer.py
@@ -1,6 +1,6 @@
 class Customer:
 
-    def __init__(self, _id:str, name:str):
+    def __init__(self, id:str, name:str):
             self.id= id
             self.name= name
             self.cart= []
